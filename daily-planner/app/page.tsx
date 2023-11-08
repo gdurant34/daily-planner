04b6feee2b815ea1d2fd141c7a46aeedfedce5d5
Grace Durant <gdurant34@gmail.com>
2023-11-07 19:35:31 -0800

    initial commit

---
+++ daily-planner/app/page.tsx
@@ -0,0 +1,16 @@
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <div className="h-full">
+      <div className=" bg-muted h-1/6 justify-center items-center flex-col gap-2">
+        <h5 className="flex-left">Day of the week</h5>
+
+      </div>
+    </div>
+  );
+}
